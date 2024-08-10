63482a4859286bef0e9e5f95e399929d6b294910
huntsfromshadow <huntsfromshadow@gmail.com>
2024-08-10 12:43:42 -0400

    Progress

--- 2021/day1/main.cpp
+++ 2021/day1/main.cpp
@@ -1,24 +1,57 @@
 #include <iostream>
 
 #include "string_utils.hpp"
+#include "vec_utils.hpp"
+#include <vector>
 
 int main()
 {
-
-    try
+ /*
+  * PART 1
+  try
     {
         auto lines = readFileLinesIntoVector("../test_input.txt");
+        auto numVec = stringVecToInt(lines);
 
-        for (auto line : lines)
-        {
-            std::cout << "~ " << line << std::endl;
+        int num_inc = 0;
+        int last_num = numVec.at(0);
+
+        std::vector<int>::iterator itr;
+        for( itr = numVec.begin(); itr != numVec.end(); itr++ ) {
+           if(*itr > last_num) {
+               num_inc++;
+           }
+           last_num = *itr;
         }
-
+        std::cout << num_inc << std::endl;
         return 0;
     }
     catch(...)
     {
         return 1;
-    }
-    readFileLinesIntoVector("tst.txt");
+    }*/
+
+
+    try
+      {
+          auto lines = readFileLinesIntoVector("../test_input.txt");
+          auto numVec = stringVecToInt(lines);
+
+          int num_inc = 0;
+          int last_num = numVec.at(0);
+
+          std::vector<int>::iterator itr;
+          for( itr = numVec.begin(); itr != numVec.end(); itr++ ) {
+             if(*itr > last_num) {
+                 num_inc++;
+             }
+             last_num = *itr;
+          }
+          std::cout << num_inc << std::endl;
+          return 0;
+      }
+      catch(...)
+      {
+          return 1;
+      }
 }
