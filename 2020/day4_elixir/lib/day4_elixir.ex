daf49acd6fc6bc1c0b1adac91622e69436871e4d
lmc <lukemccampbell@roll20.net>
2024-11-07 15:23:34 -0500

    hmm

--- 2020/day4_elixir/lib/day4_elixir.ex
+++ 2020/day4_elixir/lib/day4_elixir.ex
@@ -1,13 +1,24 @@
 defmodule Day4Elixir do
   @test_filename "test_input.txt"
-  @input_filename "day1_input.txt"
+  #@input_filename "day1_input.txt"
+
+  def break_chunk_line(dat) do
+    Enum.join(dat, " ")
+    |> convert_to_map()
+
+  end
+
+  def parse_file_list(dat) do
+    Enum.chunk_by(dat, fn x -> x != "" end)
+    |> Enum.filter(fn x -> x != [""] end)
+    |> Enum.each(&break_chunk_line/1)
+  end
 
   def part1(filename) do
     {:ok, fdata} = File.read(filename)
 
     String.split(fdata, "\n")
-    |> Enum.chunk_by(fn x -> x != "" end)
-    |> Enum.filter(fn x -> x != [""] end)
+    |> parse_file_list()
     |> IO.inspect()
 
   end
